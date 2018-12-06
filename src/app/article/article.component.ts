9a6d4d6c7ff44ec638a267f65105688d3a220392
ofentsejabari <jabariofentse@gmail.com>
2018-12-06 15:57:12 +0200

    Update

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+
+
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  @Input() article: Article;
+
+  constructor() {
+      // article is populated by the Input now,
+      // so we don't need anything here
+  }
+
+  voteUp(): boolean {
+      this.article.voteUp();
+      return false;
+  }
+  voteDown(): boolean {
+      this.article.voteDown();
+      return false;
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -9,4 +9,23 @@ export class Article {
       this.votes = votes || 0;
   }
 
+  voteUp(): void {
+    this.votes += 1;
+  }
+
+  voteDown(): void {
+    this.votes -= 1;
+  }
+
+  domain(): string {
+      try {
+          //-- e.g. http://foo.com/path/to/bar
+          const domainAndPath: string = this.link.split('//')[1];
+          //-- e.g. foo.com/path/to/bar
+          return domainAndPath.split('/')[0];
+      } catch (err) {
+          return null;
+      }
+  }
+
 }
